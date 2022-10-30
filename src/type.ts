ae18cc500e9efd5b20cb7bcd8f018282fb4a0d1a
tta-multiIT <tta@multi-it.dk>
2022-10-30 07:00:16 +0700

    update type

---
+++ src/type.ts
@@ -0,0 +1,16 @@
+export type HomeEntriesItemProps = {
+    title: string;
+    value: number;
+    date: string;
+};
+
+export type HomeMajorItemProps = {
+    title: string;
+    value: number;
+};
+
+export type NoteListItemProps = {
+    date: string;
+    title: string;
+    content: string;
+};
